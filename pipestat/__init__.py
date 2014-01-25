4e66b194dc900bf29df077ba55296bcb4e644634
timmy.yuan <timmy.yuan@netis.com.cn>
2014-01-26 01:22:04 +0800

    change version

--- pipestat/__init__.py
+++ pipestat/__init__.py
@@ -4,4 +4,4 @@ from pipestat.pipeline import Pipeline
 from pipestat.errors import PipeStatError, OperatorError, CommandError, LimitCompleted
 
 __author__ = "Timmy Yuan"
-__version__ = "0.1.8"
+__version__ = "0.2.0"
